00b32921dbbfe56b63e193d4add72349b9984584
Apocalyptapig <101874145+Apocalyptapig@users.noreply.github.com>
2022-04-15 09:31:02 -0500

    Update align_bpm.lua

--- align_bpm.lua
+++ align_bpm.lua
@@ -2,35 +2,34 @@ local all = app.activeSprite.frames
 
 local dlg = Dialog()
 dlg:entry{ id="bpm", label="Beats Per Minute", text="e.g. 100",
-	focus=true }
+    focus=true }
 dlg:button{ id="confirm", text="Confirm",
-	focus=true }
+    focus=true }
 dlg:button{ id="cancel", text="Cancel" }
 dlg:show()
 data = dlg.data
 
-if data.confirm then
-	
-	local mspf = 60000/data.bpm
-	
-	function getFrames()
-		local total = 0
-		for i, frame in ipairs(all) do 
-			app.activeFrame = i 
-			total = total + app.activeFrame.duration * 1000
-		end
-			ratio = mspf / total
-	end 
-	
-	function editFrames()
-		for i, frame in ipairs(all) do 
-			app.activeFrame = i 
-			app.activeFrame.duration = app.activeFrame.duration * ratio
-		end
-	end 
-	
-	getFrames() 
-	editFrames()
+function getFrames()
+    local mspf = 60000/data.bpm
+    local total = 0
+    for i, frame in ipairs(all) do 
+        app.activeFrame = i 
+        total = total + app.activeFrame.duration * 1000
+    end
+        ratio = mspf / total
+end
 
-	app.alert("Animation successfully aligned to " .. data.bpm .. " BPM")
+function editFrames()
+    for i, frame in ipairs(all) do 
+        app.activeFrame = i 
+        app.activeFrame.duration = app.activeFrame.duration * ratio
+    end
+end
+    
+if data.confirm then
+
+    getFrames() 
+    editFrames()
+
+    app.alert("Animation successfully aligned to " .. data.bpm .. " BPM")
 end
